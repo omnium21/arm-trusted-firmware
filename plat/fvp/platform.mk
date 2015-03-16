e628aa5d3d7d4a8e6ab527db280e06f21e02d954
Ryan Harkin <ryan.harkin@linaro.org>
2015-03-16 12:41:33 +0000

    fvp: add SP804 timer
    Add support for the SP804 timer to the FVP platform.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- plat/fvp/platform.mk
+++ plat/fvp/platform.mk
@@ -47,6 +47,7 @@ $(eval $(call add_define,FVP_TSP_RAM_LOCATION_ID))
 PLAT_INCLUDES		:=	-Iplat/fvp/include/
 
 PLAT_BL_COMMON_SOURCES	:=	drivers/arm/pl011/pl011_console.S		\
+				drivers/arm/sp804/sp804.c			\
 				drivers/io/io_fip.c				\
 				drivers/io/io_memmap.c				\
 				drivers/io/io_semihosting.c			\
